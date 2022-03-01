9343610f8889ee9b1d2c2ed5fce6f67eb9fc1035
amena <amena.ruet@gmail.com>
2022-03-01 21:52:54 +0600

    onclick detail

--- app.js
+++ app.js
@@ -17,38 +17,54 @@ const loadPhone=(searchtext)=>{
    
     fetch(url)
     .then(res=>res.json())
-    .then(phones=>displayPhone(phones.data))
+    .then(phones=>displayPhone(phones.data.slice(0,6)))
 
 }
 
 const displayPhone=(phones)=>{
     
     for(const phone of phones){
-       console.log(phone)
+       
         const parent=document.getElementById('phone-contaniner')
         const div=document.createElement('div')
             div.innerHTML=`
-                <div class="card border ">
-                        <div class="phone-pic">
-                            <img  src="" alt="">
+                <div class="card border mb-3  ">
+                        <div class="phone-pic text-center">
+                            <img class="w-100 mb-3" src="${phone.image}" alt="">
                         </div>
-                        <h4>Brand:</h4>
-                        <h4>Ph_Name:</h4>
-                        <div class="allbutton">
-                            <button class="btn btn-danger" >delete</button>
-                            <button class="btn btn-success" >details</button>
+                        
+                        <h5 class="text-center mb-3" ><span class="text-danger">Ph_Name:</span> ${phone.phone_name}</h5>
+                        <div class="allbutton text-center">
+                            <button  class="btn btn-danger" >delete</button>
+                            <button onclick="details('${phone.slug}')" class="btn btn-success" >details</button>
                         </div>
                         
                 </div>`
          
          parent.appendChild(div)
+
          
     }  
        
-   
-    
-
 }
 
+const details=(info)=>{
+    console.log(info)
+ const url=` https://openapi.programming-hero.com/api/phone/${info}`
+ fetch(url)
+ .then(res=>res.json())
+ .then(detail=>setDetails(detail.data))
+}
 
+const setDetails=(info)=>{
+  document.getElementById('detail-contaniner').innerHTML=`
+  <div>
+  <h5 class="text-center"><span class="text-info">Storage:</span> ${info.mainFeatures.storage}</h5>
+  <h5 class="text-center" ><span class="text-info">Memory:</span> ${info.mainFeatures.memory}</h5>
+  <h5 class="text-center"><span class="text-info">Slug:</span> ${info.slug}</h5>
+  <h5 class="text-center"><span class="text-info">Releasedate:</span> ${info.releaseDate}</h5>
 
+  </div>
+  `
+
+}
