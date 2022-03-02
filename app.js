032a81474e297288db6d963702e8bff3e2c9017e
amena <amena.ruet@gmail.com>
2022-03-02 21:40:43 +0600

    style

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
+//search phone
 const searchPhone=()=>{
+    document.getElementById('detail-contaniner').innerHTML="" 
     document.getElementById('phone-contaniner').innerHTML=""
-   
     document.getElementById('spinner').style.display='block'
     document.getElementById('spinner1').style.display='block'
 
@@ -19,6 +20,9 @@ const searchPhone=()=>{
     
 }
 
+
+// calling phone_name API
+
 const loadPhone=(searchtext)=>{
     const url=`https://openapi.programming-hero.com/api/phones?search=${searchtext}`
     
@@ -29,63 +33,87 @@ const loadPhone=(searchtext)=>{
         if(phones.data.length==0){
             document.getElementById('spinner').style.display='block'
             document.getElementById('spinner1').style.display='block'
-            return
+
+            //set timeout to stop loading
+            setTimeout(()=>{
+                document.getElementById('spinner').style.display ='none'
+                document.getElementById('spinner1').style.display='none'
+            }, 2000)
+
+            //set timeout showing no-found message
+           setTimeout(()=>{
+                document.getElementById("no-found").style.display="block"
+            },2000)
+            
         }
 
         else{
-            displayPhone(phones.data.slice(0,6))
-            document.getElementById('spinner').style.display='none'
-            document.getElementById('spinner1').style.display='none'
+            displayPhone(phones.data.length>20?phones.data.slice(0,20):phones.data)
+            document.getElementById("no-found").style.display="none"
         }
     })
         
-        
 }
+
+//display phone to receive phones data 
 
 const displayPhone=(phones)=>{
     
-    for(const phone of phones){
+   phones.forEach(phone => {
        
-        const parent=document.getElementById('phone-contaniner')
+    const parent=document.getElementById('phone-contaniner')
         const div=document.createElement('div')
             div.innerHTML=`
                 <div class="card border border-2 border-secondery mb-3   ">
-                        <div class="phone-pic text-center">
-                            <img class="w-75 mb-3" src="${phone.image}" alt="">
+                        <div class="phone-pic text-center ">
+                            <img class="w-75 mb-3 mt-2 " src="${phone.image}" alt="">
                         </div>
-                        
+                        <h5 class="text-center mb-3" ><span class="text-danger">Brand:</span> ${phone.brand}</h5>
                         <h5 class="text-center mb-3" ><span class="text-danger">Ph_Name:</span> ${phone.phone_name}</h5>
                         <div class="allbutton text-center">
-                            <button  class="btn btn-danger" >delete</button>
+                            
                             <button onclick="details('${phone.slug}')" class="btn btn-success" >details</button>
                         </div>
-                        
                 </div>`
          
          parent.appendChild(div)
 
-         
-    }  
-       
+    } ) 
+    document.getElementById('spinner').style.display='none'
+    document.getElementById('spinner1').style.display='none' 
 }
 
+
+// calling API by Id to display phone details 
+
 const details=(info)=>{
-    console.log(info)
- const url=` https://openapi.programming-hero.com/api/phone/${info}`
- fetch(url)
- .then(res=>res.json())
- .then(detail=>setDetails(detail.data))
+    const url=` https://openapi.programming-hero.com/api/phone/${info}`
+    fetch(url)
+    .then(res=>res.json())
+    .then(detail=>setDetails(detail.data))
 }
+
+//Set details to receive Id
 
 const setDetails=(info)=>{
   document.getElementById('detail-contaniner').innerHTML=`
-  <div>
-  <h5 class="text-center"><span class="text-info">Storage:</span> ${info.mainFeatures.storage}</h5>
-  <h5 class="text-center" ><span class="text-info">Memory:</span> ${info.mainFeatures.memory}</h5>
-  <h5 class="text-center"><span class="text-info">Slug:</span> ${info.slug}</h5>
-  <h5 class="text-center"><span class="text-info">Releasedate:</span> ${info.releaseDate}</h5>
-
-  </div>
+  <div >
+    <div class=" text-center mb-3 mt-2">
+         <img  class=" w-50 text-center " src=" ${info.image}" alt="">
+    </div>
+    <h5 class="text-center "><span class="text-danger">Storage:</span> ${info.mainFeatures.storage}</h5>
+    <h5 class="text-center" ><span class="text-danger">Memory:</span> ${info.mainFeatures.memory}</h5>
+    <h5 class="text-center"><span class="text-danger">Displaysize:</span> ${info.mainFeatures.displaySize}</h5>
+    <h5 class="text-center"><span class="text-danger">Releasedate:</span> ${info.releaseDate?info.releaseDate:'none'}</h5>
+    
+    <div class="bg-light">
+        <h4 class="text-center text-primary">Others:</h4>
+        
+        <h5 class="text-center"><span class="text-danger">WLAN:</span> ${info.others.WLAN}</h5>
+        <h5 class="text-center"><span class="text-danger">GPS:</span> ${info.others.GPS}</h5>
+        <h5 class="text-center"><span class="text-danger">USB:</span> ${info.others.USB}</h5>
+    </div>
+    </div>
   `
 
 }
